33ab949909f07f9820506754cdd693e43585ee9e
anonymous <anonymous@example.com>
2020-12-08 23:49:13 +0800

    将AuxiliaryStar改为嵌套类

--- headers/astar.h
+++ headers/astar.h
@@ -1,11 +1,29 @@
 #ifndef ASTAR_H
 #define ASTAR_H
 
-#include "auxiliarystar.h"
+#include "graph.h"
+#include "indexminpq.h"
 #include <functional>
 #include <queue>
 
 class Astar {
+private:
+    // an auxiliary class contains everything needed in Astar Algorithm
+    struct AuxiliaryStar {
+        const Graph& G;
+        std::vector<size_t> path;
+        bool* marked;
+        IndexMinPQ pq;
+        double dist;
+        double evaluate;
+
+        AuxiliaryStar(const Graph& G);
+        ~AuxiliaryStar();
+        AuxiliaryStar(const AuxiliaryStar&) = delete;
+        AuxiliaryStar& operator=(const AuxiliaryStar&) = delete;
+        AuxiliaryStar(const AuxiliaryStar& orgi, const Edge& e);
+    };
+
 private:
     static const std::function<bool(AuxiliaryStar*, AuxiliaryStar*)> cmp;
     std::priority_queue<AuxiliaryStar*, std::vector<AuxiliaryStar*>, decltype(cmp)> pq;
--- src/astar.cpp
+++ src/astar.cpp
@@ -1,9 +1,40 @@
 #include "astar.h"
 #include <stdexcept>
-const std::function<bool(AuxiliaryStar*, AuxiliaryStar*)> Astar::cmp =
+const std::function<bool(Astar::AuxiliaryStar*, Astar::AuxiliaryStar*)> Astar::cmp =
     [](AuxiliaryStar* lhs, AuxiliaryStar* rhs) -> bool {
     return lhs->evaluate > rhs->evaluate;
 };
+
+// nested auxiliary class
+Astar::AuxiliaryStar::AuxiliaryStar(const Graph& G)
+    : G(G), marked(new bool[ G.sizeV() ]), pq(G.sizeE()), dist(0), evaluate(0) {
+    for (size_t i = 1; i < G.sizeV(); ++i)
+        marked[ i ] = false;
+    marked[ 0 ] = true;
+    path.push_back(0);
+    for (size_t i = G.sizeV() - 1; i < G.sizeE(); ++i)
+        pq.insert(i, G.getEdge(i).weight());
+}
+Astar::AuxiliaryStar::~AuxiliaryStar() { delete[] marked; }
+Astar::AuxiliaryStar::AuxiliaryStar(const AuxiliaryStar& orgi, const Edge& e)
+    : G(orgi.G), path(orgi.path), marked(new bool[ G.sizeV() ]), pq(orgi.pq),
+      dist(orgi.dist), evaluate(orgi.evaluate) {
+    std::copy(orgi.marked, orgi.marked + G.sizeV(), marked);
+    size_t v = path.back(), w = e.other(v);
+    if (marked[ w ]) throw std::invalid_argument("already in path");
+    path.push_back(w);
+    marked[ w ] = true;
+    dist += e.weight();
+    if (path.size() == orgi.G.sizeV()) {
+        const Edge& e = G.between(w, 0);
+        evaluate = dist = dist + e.weight();
+        return;
+    }
+    evaluate = dist + (G.sizeV() - path.size() + 1) * pq.minElem();
+    for (const size_t& e : G.adj(w))
+        pq.delElem(e);
+}
+
 Astar::Astar(const Graph& G) : pq(cmp) {
     AuxiliaryStar* t = new AuxiliaryStar(G);
     pq.push(t);
